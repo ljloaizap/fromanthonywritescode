ba5e56f3f900926f5923fa72d0afd7c1bdd7bd75
Liliana Loaiza <liliana.loaiza@globant.com>
2023-04-29 17:46:16 -0500

    :new: Add explain/54 functools.lru_cache

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # From AnthonyWritesCode
 This repo is my journey coding along Anthony with his content in [AnthonyWritesCode Youtube Channel](https://www.youtube.com/@anthonywritescode)
 
+# Cache
+## @functools.lru_cache decorator
+You can cache the results of a function and play with the size according to the news. More details in `explains/54_functools.lru_cache.py` module.
+
+
 # Useful packages
 - `mypy` -> [link](https://pypi.org/project/mypy/)
 - _anthonywritescode_, `explains` [Github repo](https://github.com/anthonywritescode/explains).
---
+++ explains/54_functools.lru_cache.py
@@ -0,0 +1,33 @@
+""" YT: https://www.youtube.com/watch?v=K0Q5twtYxWY&ab_channel=anthonywritescode
+
+Note: in case you need to run the function again to test it it's working properly,
+without the need to modify source code to remove decorator, you can call the 
+underline function by accessing the __wrapped__.
+"""
+
+import functools
+
+
+@functools.lru_cache
+def square(number: float) -> float:
+    """PH"""
+    print(f'Running: {number}')
+    return number * number
+
+
+@functools.lru_cache(3)
+def square_max_size(number: float) -> float:
+    """PH"""
+    print(f'Running: {number}')
+    return number * number
+
+
+@functools.lru_cache(maxsize=1)
+def get_expensive_thing() -> int:
+    """
+        Let's pretend this is an expensive thing. Parameter 'maxsize' is set to 1 
+        because it will ALWAYS return the same value, so there's no need to allocate 
+        more resources in cache for more values (wasteful!). Some sort of 'Singleton'.
+    """
+    print('Running expensive code...')
+    return 7
