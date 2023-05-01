4efacdeecf4048f00e1f3edf1d4c30696e066c7f
Liliana Loaiza <liliana.loaiza@globant.com>
2023-05-01 17:50:43 -0500

    :new: Add explains/246 Pytest output

---
+++ explains/246_pytest_output.py
@@ -0,0 +1,54 @@
+"""YT: https://youtu.be/dN-pVt7i4Us
+How to test output using pytest -- using the `capsys` and `capfd` fixtures (and why they're different)
+"""
+import subprocess
+import sys
+
+# region source_code
+
+
+def hello(name: str) -> None:
+    """PH"""
+    print(f'hello hello {name}')
+
+
+def hello_subprocess(name: str) -> None:
+    """PH"""
+    subprocess.check_call(('echo', 'hello', 'hello', name))
+
+
+def main() -> int:
+    """PH"""
+    # hello(sys.argv[1])
+    hello_subprocess(sys.argv[1])
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+# endregion
+
+# region tests
+
+
+def test_hello_capsys(capsys):
+    """PH"""
+    hello('lili')
+    stdout, stderr = capsys.readouterr()
+    assert stdout == 'hello hello lili\n'
+
+
+# def test_hello_capsys_subprocess(capsys):
+#     """This test fails because capsys doesn't capture what subprocess prints in console"""
+#     hello_subprocess('lolo')
+#     stdout, stderr = capsys.readouterr()
+#     assert stdout == 'hello hello lolo\n'
+
+def test_hello_capfd(capfd):
+    """PH"""
+    hello('michi')
+    stdout, _ = capfd.readouterr()
+    assert stdout == 'hello hello michi\n'
+
+
+# endregion
